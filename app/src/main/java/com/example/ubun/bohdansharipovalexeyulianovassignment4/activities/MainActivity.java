8cf505feac4cd84e2dc3fb430f0969d508e4ecc4
Bohdansh <bohdansharipov@gmail.com>
2017-12-06 22:14:16 -0500

    Added db build

--- app/src/main/java/com/example/ubun/bohdansharipovalexeyulianovassignment4/activities/MainActivity.java
+++ app/src/main/java/com/example/ubun/bohdansharipovalexeyulianovassignment4/activities/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.ubun.bohdansharipovalexeyulianovassignment4.activities;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.ubun.bohdansharipovalexeyulianovassignment4.R;
+import com.example.ubun.bohdansharipovalexeyulianovassignment4.database.MyDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    public MyDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (db == null) {
+            db = Room.databaseBuilder(getApplicationContext(),
+                    MyDatabase.class, "database-name").build();
+        }
     }
 }
